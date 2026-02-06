d14b03e5482e02d5fe809bcc04a5347cddb25fb6
Froggkrl <kir07kot@gmail.com>
2026-02-06 19:51:58 +0300

    ver01

--- cmd/bank/main.go
+++ cmd/bank/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -11,17 +13,50 @@ type Account struct {
 	Balance float64
 }
 
+var accounts = make(map[string]Account)
+var currentAccount *Account
+
 func main() {
 	fmt.Print(strings.Repeat("&===& ", 2))
 	fmt.Print("Bank CLI Go")
 	fmt.Println(strings.Repeat(" &===& ", 2))
 
-	testAccount := Account{
-		ID:      "A001",
+	CreateDemoAcc()
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		printMenu()
+		fmt.Println("Выберите действие:")
+		scanner.Scan()
+		choice := strings.TrimSpace(scanner.Text())
+		switch choice {
+		case "1":
+			viewInfo()
+		}
+
+	}
+}
+
+func CreateDemoAcc() {
+	account := Account{
+		ID:      generateID(),
 		Name:    "Ivan",
 		Balance: 1000.0,
 	}
+	accounts[account.ID] = account
+	currentAccount = &account
+	fmt.Printf("Создан демо-счет %s\n", account.ID)
+}
 
-	fmt.Printf("Здравствуйте вот ваши данные:\n ID: %s\n Name: %s\n Balance: %.2f\n ", testAccount.ID, testAccount.Name, testAccount.Balance)
+func generateID() string {
+	return fmt.Sprintf("ACC%d", len(accounts)+1)
+}
 
+func viewInfo() {
+	fmt.Printf(`Здравствуйте вот ваши данные:\n ID: %s\n Name: %s\n Balance: %.2f\n `,
+		currentAccount.ID, currentAccount.Name, currentAccount.Balance)
+}
+
+func printMenu() {
 }
